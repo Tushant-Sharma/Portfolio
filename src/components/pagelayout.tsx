59a2cb4520e5fcedd3c03093e3edd0f94d41e134
Tushant Sharma <info.tushant@gmail.com>
2024-03-07 14:38:53 +0530

    scroll change

--- src/components/pagelayout.tsx
+++ src/components/pagelayout.tsx
@@ -50,7 +50,7 @@ const Header = () => {
                     <div className="gx-row d-flex align-items-center justify-content-between">
                         <div className="logo" style={navLogo}>
                             {
-                                canGoBack && location.pathname !== '/home' ? <><span style={{ color: "#fff", cursor: "pointer", marginTop: "-10px", textDecoration: "overline" }} onClick={() => { goBack(-1) }} >
+                                canGoBack && location.pathname !== '/home' ? <><span style={{ color: "#fff", cursor: "pointer", textDecoration: "overline" }} onClick={() => { goBack(-1) }} >
 
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m12 19-7-7 7-7" /><path d="M19 12H5" /></svg>
                                 </span>
@@ -58,7 +58,7 @@ const Header = () => {
                             }
 
                             <Link to="/">
-                                <h1 className="title">TS</h1>
+                                <h1 className="title" style={{ margin: "0 0 0 0!important" }}>TS</h1>
                             </Link>
                             {/* <img src="../src/assets/images/logo.svg" alt="Logo" /> */}
                         </div>
