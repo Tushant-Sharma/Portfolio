afe608a1a800af1ec3df3bc081bd2a98df1cd1a2
Tushant Sharma <info.tushant@gmail.com>
2024-07-04 08:40:54 +0530

    new resume update

--- src/components/pagelayout.tsx
+++ src/components/pagelayout.tsx
@@ -110,7 +110,7 @@ const Footer = () => {
                         <li><Link to="about">About </Link></li>
                         <li><Link to="blogs">Blogs</Link></li>
 
-                        <li><Link target="_blank" to={socialLinks.resume}>CV_Download</Link></li>
+                        {/* <li><Link target="_blank" to={socialLinks.resume}>CV_Download</Link></li> */}
                         <li><Link to="contact">Contact</Link></li>
                     </ul>
                     <p className="copyright">
